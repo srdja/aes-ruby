010467cdfa5c67bf4fb84b381c747dd039ca79bd
srdja <i@srdja.me>
2016-02-15 15:24:06 +0100

    Implementing ecb mode decryption

--- src/aes_ecb.rb
+++ src/aes_ecb.rb
@@ -27,9 +27,9 @@ class AES_ECB
     end
 
     blocks = msg.length / b_size
-    cipher_text = []
 
-    cipher_text.push(*@cipher.encrypt_block(msg[(0..(b_size - 1))], key))
+    cipher_text = @cipher.encrypt_block(msg[(0..(b_size - 1))], key)
+
     (1..(blocks - 1)).each do |b|
       b_range = ((b_size * b)..(b_size * (b + 1) - 1))
       cipher_text.push(*(@cipher.encrypt_block(msg[b_range], key)))
@@ -39,8 +39,37 @@ class AES_ECB
   end
 
 
-  def decrypt(cipher_text, key)
+  def decrypt(msg, key, padded)
+    b_size = @cipher.block_size
 
+    if msg.length % b_size != 0
+      raise ArgumentError.new("Message size is not a multiple of the block size")
+    end
+
+    blocks = msg.length / b_size
+
+    plain_text = @cipher.decrypt_block(msg[(0..(b_size - 1))], key)
+    (1..(blocks - 1)).each do |b|
+      b_range = ((b_size * b)..(b_size * (b + 1) - 1))
+      plain_text.push(*(@cipher.decrypt_block(msg[b_range], key)))
+    end
+
+    # Remove padding if the message was padded
+    if padded
+      p_len = 0
+      (msg.length - 1).step(0, -1) do |b|
+        byte = msg[i]
+        p_len += 1
+        if byte == 0x80
+          break
+        elsif byte != 0x00
+          raise ArgumentError.new("No padding found")
+        end
+      end
+      x.pop(p_len)
+    end
+
+    plain_text
   end
 
 end
